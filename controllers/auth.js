9f4e9d2cb6ba9a3a067418063bc850bf2f939556
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-06-24 17:36:00 -0500

    :bug: Handle school_audit_id as part of registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,16 +7,22 @@ const { checkToken, checkRole } = require("../middlewares/authorization");
 
 // Models
 const User = require('../models/db/user')
+const School = require('../models/db/school')
 
 router.route('/register')
   .post(async (req, res) => {
     const user = {
-      ...req.body,
-      password: bcrypt.hashSync(req.body.password, 10)
+      ...req.body.user,
+      password: bcrypt.hashSync(req.body.user.password, 10)
     }
+    const { school_audit_id } = req.body
 
     try {
-      const newUser = await User.add(user)
+      const { id: school_id} = await School.find({ 
+        's.audit_id': school_audit_id 
+      }, { internal: true }).first()
+    
+      const newUser = await User.add({...user, school_id })
 
       return res.status(201).json({
         message: `User ${newUser.username} has been created`
@@ -27,6 +33,7 @@ router.route('/register')
         `Error during ${req.method} @ ${req.path}\n\n`,
         `Error: ${e}\n\n`
       );
+      console.error(e)
       return res.status(500).json({ 
         message: "We encountered an error during registration" 
       });
@@ -37,12 +44,12 @@ router.route('/login')
   .post(async (req, res) => {
     try {
       const { username, password } = req.body;
-      console.log(username, password)
+  
       const user = await User.find({ 'u.username': username }).first()
-      console.log(user)
+
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
-        console.log(token)
+
         return res.status(200).json({
           message: `Welcome ${user.username}`,
           token
