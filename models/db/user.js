7f3ba6929137fb580ad28d509e1cd5c64c015bf5
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-06-24 11:20:33 -0500

    :card_file_box: Create User model

---
+++ models/db/user.js
@@ -0,0 +1,40 @@
+const db = require('../index');
+
+module.exports = {
+  add,
+  find,
+  update,
+  remove
+}
+
+function add(user) {
+  return db('users')
+    .insert(user, ['*'])
+    .then(u => find({ 'u.id': u[0].id }).first())
+}
+
+function find(filters) {
+  return db('users AS u')
+    .select(
+      'u.audit_id AS audit_id',
+      'u.username AS username',
+      'u.avatar_url AS avatar',
+      'u.is_admin AS is_admin',
+      's.name AS school'
+    )
+    .join('school AS s', { 's.id': 'u.school_id' })
+    .where(filters)
+}
+
+function update(filters, changes) {
+  return db('users AS u')
+    .update(changes, ['*'])
+    .where(filters)
+    .then(u => find({ 'u.id': u[0].id }).first())
+}
+
+function remove(filters) {
+  return db('users AS u')
+    .where(filters)
+    .del()
+}
